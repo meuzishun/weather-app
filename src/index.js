5e56fca8e0edceeefc590b3a8fb557d89ff9fc41
Andrew Smith <meuzishun@gmail.com>
2022-01-09 19:10:41 -0500

    write functions for calling the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import {
-  getCurrentWeather,
-  extractRelavantData,
-  oneCall,
-} from './js/weatherAPI.js';
-import { renderLocationForm, renderDayCard } from './js/UI.js';
-import { temperatureConversion } from './js/utilities.js';
+import * as weatherAPI from './js/weatherAPI.js';
 
-// createLocationForm();
+(async function () {
+  // const coords = await weatherAPI.getLocationFromZip('01801');
+  // const { lat, lon } = coords;
+  // const locations = await weatherAPI.getLocationsFromCoords(lat, lon, 1);
+  // const { name, state, country } = locations[0];
+  // const weatherData = await weatherAPI.getWeatherFromCoords(lat, lon);
+  const locationsFromSearch = await weatherAPI.getLocationsFromNames('boston', 'Massachusetts');
+  console.log(locationsFromSearch);
+})();
--- src/js/weatherAPI.js
+++ src/js/weatherAPI.js
@@ -1,79 +1,51 @@
-import { temperatureConversion } from "./utilities.js";
+// store the API key
+const API_key = 'd4a3732932608b542cb92d60253a6c4f';
 
-const getCurrentWeather = async function (location) {
-  try {
-    const API_key = 'd4a3732932608b542cb92d60253a6c4f';
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_key}`;
-    const response = await fetch(url, { mode: 'cors' });
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-const getLocations = async function (searchValue) {
-  try {
-    const API_key = 'd4a3732932608b542cb92d60253a6c4f';
-    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${searchValue}&limit=5&appid=${API_key}`;
-    const response = await fetch(url, { mode: 'cors' });
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-
-}
-
-const oneCall = async function (lon, lat) {
-  try {
-    const API_key = 'd4a3732932608b542cb92d60253a6c4f';
-    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_key}`;
-    const response = await fetch(url, { mode: 'cors' });
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-const extractDailyCardData = function(summary) {
-  const today = new Date();
-  const weekdayLookup = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
-  ];
-  const dailyCards = summary.daily.map((day, index) => {
-    if (index < 6) {
-      const weekday = weekdayLookup[(today.getDay() + index) % 6];
-      const icon = day.weather[0].icon;
-      // const temp = `${Math.round((((day.temp.day - 273.15) * 9) / 5) + 32)}º`;
-      const temp = `${Math.round(temperatureConversion('fahrenheit', 'kelvin', day.temp.day))}º`;
-      const description = day.weather[0].description;
-      return { weekday, icon, temp, description };
-    }
-  });
-  dailyCards.length = 6;
-  return dailyCards;
-}
-
-function extractRelavantData(data) {
-  const importantData = {
-    name: data.name,
-    weather: data.weather,
-  };
-  return importantData;
-}
-
-export {
-  getCurrentWeather,
-  getLocations,
-  oneCall,
-  extractRelavantData,
-  extractDailyCardData,
+// write a function that fetches a location based on zipcode
+export const getLocationFromZip = async function (zip_code, country_code) {
+  const response = await fetch(
+    `http://api.openweathermap.org/geo/1.0/zip?zip=${zip_code}${
+      country_code ? ',' + country_code : ''
+    }&appid=${API_key}`,
+    { mode: 'cors' }
+  );
+  const data = await response.json();
+  return data;
 };
+
+// write a function that fetches locations from coords
+export const getLocationsFromCoords = async function (lat, lon, limit) {
+  const response = await fetch(
+    `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=${limit}&appid=${API_key}`,
+    { mode: 'cors' }
+  );
+  const data = await response.json();
+  return data;
+};
+
+// write a function that fetches locations from names
+export const getLocationsFromNames = async function (city_name, state_code, country_code) {
+  const response = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city_name}${state_code ? ',' + state_code : ''}${country_code ? ',' + country_code : ''}&limit=5&appid=${API_key}`,
+    { mode: 'cors' }
+  );
+  const data = await response.json();
+  return data;
+}
+
+// write a function that fetches weather data from coords
+export const getWeatherFromCoords = async function (lat, lon) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_key}`,
+    { mode: 'cors' }
+  );
+  const data = await response.json();
+  return data;
+};
+
+// write a function that grabs the user input
+
+// write a function that verifies the user input
+
+// write a function that checks the user input for specificity
+// if the user input is a zipcode
