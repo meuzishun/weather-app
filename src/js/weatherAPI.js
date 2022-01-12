fef482eafb74ece87c151135b076d46795bc1c54
Andrew Smith <meuzishun@gmail.com>
2022-01-12 05:29:04 -0500

    add try/catch to api calls

--- src/js/weatherAPI.js
+++ src/js/weatherAPI.js
@@ -1,23 +1,31 @@
 const API_key = 'd4a3732932608b542cb92d60253a6c4f';
 
 export const getLocationFromZip = async function (zip_code, country_code) {
-  const response = await fetch(
-    `http://api.openweathermap.org/geo/1.0/zip?zip=${zip_code}${
-      country_code ? ',' + country_code : ''
-    }&appid=${API_key}`,
-    { mode: 'cors' }
-  );
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/geo/1.0/zip?zip=${zip_code}${
+        country_code ? ',' + country_code : ''
+      }&appid=${API_key}`,
+      { mode: 'cors' }
+    );
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const getLocationsFromCoords = async function (lat, lon, limit) {
-  const response = await fetch(
-    `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=${limit}&appid=${API_key}`,
-    { mode: 'cors' }
-  );
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=${limit}&appid=${API_key}`,
+      { mode: 'cors' }
+    );
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const getLocationsFromNames = async function (
@@ -25,21 +33,29 @@ export const getLocationsFromNames = async function (
   state_code,
   country_code
 ) {
-  const response = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city_name}${
-      state_code ? ',' + state_code : ''
-    }${country_code ? ',' + country_code : ''}&limit=5&appid=${API_key}`,
-    { mode: 'cors' }
-  );
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city_name}${
+        state_code ? ',' + state_code : ''
+      }${country_code ? ',' + country_code : ''}&limit=5&appid=${API_key}`,
+      { mode: 'cors' }
+    );
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const getWeatherFromCoords = async function (lat, lon) {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_key}`,
-    { mode: 'cors' }
-  );
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_key}`,
+      { mode: 'cors' }
+    );
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 };
